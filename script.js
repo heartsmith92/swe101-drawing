5ceccc2edfc75f36f87ccbb6a9f1767676df1be4
Albus Muhammad <mdfirdausthen92@gmail.com>
2020-08-27 20:49:49 +0800

    template code

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Please declare functions and variables above where they are used.
 
 var main = function (input) {
-  var myOutputValue = 'hello world';
+
+  var myOutputValue = '';
+  var counter = 0;
+
+  while (counter < input) { //outer loop 
+
+    var innerCounter = 0;
+
+    while (innerCounter < input * 2) { //inner loop 
+
+      myOutputValue = myOutputValue + 'hello '
+
+      innerCounter += 1
+    }
+    myOutputValue = myOutputValue + '<br>'
+    counter = counter + 1;
+  }
+
   return myOutputValue;
 };
+
+
